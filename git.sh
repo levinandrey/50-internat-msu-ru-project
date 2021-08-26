dfda4f9ca51dc5b14f018ab9d7826e6089518f3d
root <root@all-internat-msu-ru.ru-central1.internal>
2021-08-26 20:46:05 +0300

    2021-08-26 20:46:05 186060859

--- git.sh
+++ git.sh
@@ -4,7 +4,7 @@ date=`date +"%Y-%m-%d %T %N"`
 echo $date
 
 sudo git add . 
-sudo git commit -m "$date" --amend --author "levinandrey <letlevin@gmail.com>"
+sudo git commit -m "$date"
 
 git push --force https://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@github.com/levinandrey/50-internat-msu-ru-project.git
 
